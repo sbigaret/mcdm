3c62d70b1f869a149b357e178aa18b93619cffe4
mateusz <fd>
2016-10-11 00:52:31 +0200

    Działający order clustering

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ K = 4
 A = ['a', 'b', 'c', 'd']
 Pi = [[0 for i in range(K)] for j in range(K)]
 
-Pi[0][1] = 1
-Pi[0][2] = 1
-Pi[0][3] = 1
-Pi[1][2] = 0.5
-Pi[1][3] = 1
+Pi[0][1] = 1.05
+Pi[0][2] = 1.04
+Pi[0][3] = 1.03
+Pi[1][2] = 0.51
+Pi[1][3] = 1.02
 Pi[2][1] = 0.5
-Pi[2][3] = 1
+Pi[2][3] = 1.01
 
 
 def OrderedClustering(index, pi, size):
@@ -31,11 +31,13 @@ def OrderedClustering(index, pi, size):
     max = FindMax(pi)
     while max[0] > 0:
         M[max[1]][max[2]] = 1
-        if Cycle(M) or 2 >= size:
-            M[max[1]][max[2]] = 0
-            I[max[1]][max[2]] = pi[max[1]][max[2]]
+        if Cycle(M) or not DFS(M, 0, 0, size-1):
+                M[max[1]][max[2]] = 0
+                I[max[1]][max[2]] = pi[max[1]][max[2]]
         pi[max[1]][max[2]] = 0
         max = FindMax(pi)
+
+    return M
 
 
 def FindMax(tab):
@@ -58,34 +60,49 @@ arr = [[0 for i in range(K)] for j in range(K)]
 arr[0][1] = 1
 arr[1][2] = 1
 arr[2][3] = 1
-arr[3][0] = 1
+arr[0][3] = 1
 
 def Cycle(G):
-    visited = { u : 0 for u in G  }
-    found_cycle = [False]
-    for u in G:
+    visited = [ 0 for g in range(len(G))]
+    found_cycle = False
+    for u in range(len(G)):
         if visited[u] == 0:
-            dfs_visit(G, u, visited, found_cycle)
-        if found_cycle[0]:
+            found_cycle, visited = dfs_visit(G, u, visited, found_cycle)
+        if found_cycle:
             break
-    return found_cycle[0]
+    return found_cycle
 
 
 def dfs_visit(G, u, visited, found_cycle):
-    if found_cycle[0]:
-        return
+    if visited[u] == 1 or found_cycle:
+        return True, visited
     visited[u] = 1
-    for v in G[u]:
-        if visited[v] == 1:
-            found_cycle[0] = True
-            return
-        if visited[v] == 0:                 
-            dfs_visit(G, v, visited, found_cycle)
-    visited[u] = 2
+    for g in range(len(G)):
+        if G[u][g] == 1:
+            result, visited = dfs_visit(G, g, visited, found_cycle)
+            if result:
+                return True, visited
+    visited[u] = 0
+    return False, visited
 
 
-print Cycle(arr)
+def DFS(G, n, length, max):
+    result = True
+    if length >= max:
+        return False
+    for k in range(len(G)):
+        if G[n][k] == 1:
+            length += 1
+            result = DFS(G, k, length, max)
+        if not result:
+            break
+    return result
 
+
+
+#print Cycle(arr)
+#print DFS(arr, 0, 0, 2)
 
 print OrderedClustering(A, Pi, K)
-print FindMax(Pi)
+
+print not DFS([[0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 0]], 0, 0, 3)

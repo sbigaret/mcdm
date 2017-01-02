5cef7f916ccab063cd646c957ac37f1aecece1fd
mateusz <fd>
2017-01-02 20:40:20 +0100

    Refactor #2

--- PrometheeV/src/org/PromV/PrometheeV.java
+++ PrometheeV/src/org/PromV/PrometheeV.java
@@ -9,10 +9,8 @@ import org.xmcda.AlternativesLinearConstraints;
 import org.xmcda.AlternativesValues;
 import org.xmcda.LinearConstraint;
 import org.xmcda.XMCDA;
-import org.xmcda.converters.v2_2_1_v3_0.AlternativeConverter;
-import org.xmcda.converters.v2_2_1_v3_0.XMCDAConverter;
-import org.xmcda.parsers.xml.xmcda_3_0.XMCDAParser;
-import org.xmcda.v2_2_1.*;
+import org.xmcda.converters.v2_v3.XMCDAConverter;
+import org.xmcda.parsers.xml.xmcda_v3.XMCDAParser;
 import scpsolver.constraints.*;
 import scpsolver.lpsolver.LinearProgramSolver;
 import scpsolver.lpsolver.SolverFactory;
@@ -25,78 +23,62 @@ public class PrometheeV {
     private ArrayList<String> alternatives;
     private XMCDA xmcda;
 
-    public enum xmcdaVersion {V2, V3};
+    public enum xmcdaVersion {V2, V3}
 
-    public PrometheeV()
-    {
+    ;
+
+    public PrometheeV() {
         xmcda = new XMCDA();
     }
 
-    public boolean LoadFile(String path, String ... typeTag)
-    {
-        final org.xmcda.parsers.xml.xmcda_3_0.XMCDAParser parser = new org.xmcda.parsers.xml.xmcda_3_0.XMCDAParser();
+    public boolean LoadFile(String path, String... typeTag) {
+        final org.xmcda.parsers.xml.xmcda_v3.XMCDAParser parser = new org.xmcda.parsers.xml.xmcda_v3.XMCDAParser();
         File file = new File(path);
 
-        if(!file.exists())
-        {
+        if (!file.exists()) {
             return false;
         }
 
-        try
-        {
+        try {
             parser.readXMCDA(xmcda, file, typeTag);
             return true;
-        }
-        catch (Throwable throwable)
-        {
+        } catch (Throwable throwable) {
             return false;
         }
     }
 
-    public org.xmcda.v2_2_1.XMCDA LoadFileObsolete(org.xmcda.v2_2_1.XMCDA xmcdaV2, String path, String ... typeTag)
-    {
+    public org.xmcda.v2.XMCDA LoadFileObsolete(org.xmcda.v2.XMCDA xmcdaV2, String path, String... typeTag) {
         File file = new File(path);
 
-        if(file.exists())
-        {
-            try
-            {
-                //return org.xmcda.parsers.xml.xmcda_2_2_1.XMCDAParser.readXMCDA(path, typeTag);
-                xmcdaV2.getProjectReferenceOrMethodMessagesOrMethodParameters().addAll(org.xmcda.parsers.xml.xmcda_2_2_1.XMCDAParser.readXMCDA(file).getProjectReferenceOrMethodMessagesOrMethodParameters());
-            }
-            catch (Throwable thr)
-            {
+        if (file.exists()) {
+            try {
+                xmcdaV2.getProjectReferenceOrMethodMessagesOrMethodParameters().addAll(org.xmcda.parsers.xml.xmcda_v2.XMCDAParser.readXMCDA(file).getProjectReferenceOrMethodMessagesOrMethodParameters());
+            } catch (Throwable thr) {
                 System.out.print(thr.getMessage());
             }
         }
         return xmcdaV2;
     }
 
-    protected HashMap<String, Double>  GetFlows()
-    {
+    protected HashMap<String, Double> GetFlows() {
         org.xmcda.AlternativesValues flows;
 
         HashMap<String, Double> result = new HashMap<String, Double>();
 
-        try
-        {
+        try {
             flows = xmcda.alternativesValuesList.get(0).asDouble();
 
-        }
-        catch (Throwable ce)
-        {
+        } catch (Throwable ce) {
             return null;
         }
 
         Set<Alternative> asd = flows.getAlternatives();
-        for( org.xmcda.Alternative alt : asd)
-        {
+        for (org.xmcda.Alternative alt : asd) {
             try {
                 LabelledQValues<QualifiedValue<Double>> something = (LabelledQValues<QualifiedValue<Double>>) flows.get(alt);
                 Object obj = something.get(0).getValue();
-                result.put(alt.id(), (double)obj);
-            }
-            catch (Throwable thr) {
+                result.put(alt.id(), (double) obj);
+            } catch (Throwable thr) {
                 return null;
             }
         }
@@ -105,28 +87,22 @@ public class PrometheeV {
         return result;
     }
 
-    protected boolean GetAlternatives()
-    {
-        try
-        {
+    protected boolean GetAlternatives() {
+        try {
             alternatives = xmcda.alternatives.getIDs();
             return true;
-        }
-        catch(Throwable thr)
-        {
+        } catch (Throwable thr) {
             return false;
         }
     }
 
-    protected void LoadData(xmcdaVersion version,  String input)
-    {
+    protected void LoadData(xmcdaVersion version, String input) {
         if (version == xmcdaVersion.V3) {
             LoadFile(input.concat("/flows.xml"), "alternativesValues");
             LoadFile(input.concat("/constraints.xml"), "alternativesLinearConstraints");
             LoadFile(input.concat("/alternatives.xml"), "alternatives");
-        }
-        else{
-            org.xmcda.v2_2_1.XMCDA xmcdaV2 = new org.xmcda.v2_2_1.XMCDA();
+        } else {
+            org.xmcda.v2.XMCDA xmcdaV2 = new org.xmcda.v2.XMCDA();
             xmcdaV2 = LoadFileObsolete(xmcdaV2, input.concat("/flows.xml"), "alternativesValues");
             xmcdaV2 = LoadFileObsolete(xmcdaV2, input.concat("/alternatives.xml"), "alternatives");
             xmcdaV2 = LoadFileObsolete(xmcdaV2, input.concat("/constraints.xml"), "alternativesLinearConstraints");
@@ -134,8 +110,7 @@ public class PrometheeV {
         }
     }
 
-    public boolean Compute(xmcdaVersion version, String input, String output)
-    {
+    public boolean Compute(xmcdaVersion version, String input, String output) {
         LoadData(version, input);
 
         if (!GetAlternatives())
@@ -148,69 +123,60 @@ public class PrometheeV {
         if (!MakeConstraints(lp))
             return false;
 
-        if (!SolveAndSave(lp, output))
+        if (!SolveAndSave(xmcdaVersion.V2, lp, output))
             return false;
 
         return true;
     }
 
 
-    private LinearProgram MakeLP()
-    {
+    private LinearProgram MakeLP() {
         flows = GetFlows();
         if (flows == null)
             return null;
-        double [] problem = new double[flows.size()];
+        double[] problem = new double[flows.size()];
         Arrays.fill(problem, 1.0);
         LinearProgram lp = new LinearProgram(problem);
         lp.setMinProblem(false);
 
-        for(int i = 0; i < alternatives.size(); i++)
+        for (int i = 0; i < alternatives.size(); i++)
             lp.setBinary(i);
 
         return lp;
     }
 
-    private boolean MakeConstraints(LinearProgram lp)
-    {
-        try
-        {
+    private boolean MakeConstraints(LinearProgram lp) {
+        try {
             org.xmcda.AlternativesLinearConstraints constraints = xmcda.alternativesLinearConstraintsList.get(0);
 
-            ListIterator<LinearConstraint> var =  constraints.listIterator();
+            ListIterator<LinearConstraint> var = constraints.listIterator();
 
-            while(var.hasNext())
-            {
+            while (var.hasNext()) {
                 LinearConstraint linearConstr = var.next();
 
                 String name = linearConstr.id();
-                double rhs = (double)linearConstr.getRhs().getValue();
+                double rhs = (double) linearConstr.getRhs().getValue();
                 double[] leftSide = new double[alternatives.size()];
                 Arrays.fill(leftSide, 0.0);
 
                 ArrayList<LinearConstraint.Element> elements = linearConstr.getElements();
-                for(LinearConstraint.Element ele : elements)
-                {
+                for (LinearConstraint.Element ele : elements) {
                     Alternative owner = (Alternative) ele.getUnknown();
-                    leftSide[ alternatives.indexOf(owner.id()) ] = (double)ele.getCoefficient().getValue();
+                    leftSide[alternatives.indexOf(owner.id())] = (double) ele.getCoefficient().getValue();
                 }
 
                 LinearConstraint.Operator op = linearConstr.getOperator();
                 ConstraintFactory(leftSide, op.name(), rhs, name, lp);
 
             }
-        }
-        catch(Throwable thr)
-        {
+        } catch (Throwable thr) {
             return false;
         }
         return true;
     }
 
-    private void ConstraintFactory(double[] leftSide, String operator, double rhs, String name, LinearProgram lp)
-    {
-        switch (operator)
-        {
+    private void ConstraintFactory(double[] leftSide, String operator, double rhs, String name, LinearProgram lp) {
+        switch (operator) {
             case "GEQ":
                 lp.addConstraint(new LinearBiggerThanEqualsConstraint(leftSide, rhs, name));
                 break;
@@ -226,29 +192,35 @@ public class PrometheeV {
         }
     }
 
-    private boolean SolveAndSave(LinearProgram lp, String out)
-    {
+    private boolean SolveAndSave(xmcdaVersion version, LinearProgram lp, String out) {
         LinearProgramSolver solver = SolverFactory.newDefault();
         double[] sol = solver.solve(lp);
         xmcda.alternativesValuesList.clear();
 
-        for (int i = 0; i < alternatives.size(); i++)
-        {
+        for (int i = 0; i < alternatives.size(); i++) {
             Alternative alt = new Alternative(alternatives.get(i));
             AlternativesValues<Double> altV = new AlternativesValues<Double>();
             altV.setDefault(alt, sol[i]);
             xmcda.alternativesValuesList.add(altV);
         }
 
-        final XMCDAParser parser = new XMCDAParser();
-        final File plik = new File(out, "result.xml");
-        try
-        {
-            parser.writeXMCDA(xmcda, plik.getAbsolutePath(), "alternativesValues");
+        if (version == xmcdaVersion.V3) {
+            final XMCDAParser parser = new XMCDAParser();
+            final File plik = new File(out, "result.xml");
+            try {
+                parser.writeXMCDA(xmcda, plik.getAbsolutePath(), "alternativesValues");
+            } catch (Throwable thr) {
+                return false;
+            }
         }
-        catch (Throwable thr)
-        {
-            return false;
+        else {
+            org.xmcda.v2.XMCDA resXmcda = XMCDAConverter.convertTo_v2(xmcda);
+            final File plik = new File(out, "result.xml");
+            try {
+                org.xmcda.parsers.xml.xmcda_v2.XMCDAParser.writeXMCDA(resXmcda, plik.getAbsolutePath(), "alternativesValues");
+            } catch (Throwable thr) {
+                return false;
+            }
         }
 
         return true;

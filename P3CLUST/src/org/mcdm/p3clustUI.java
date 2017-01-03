a3fef90b66a7be11705cbd8efd9d1e9952924575
mateusz <fd>
2017-01-03 19:30:45 +0100

    Support V2 (P3CLUST)

--- P3CLUST/src/org/mcdm/p3clustUI.java
+++ P3CLUST/src/org/mcdm/p3clustUI.java
@@ -5,6 +5,6 @@ public class p3clustUI {
     public static void main(String[] args)
     {
         P3clust test = new P3clust();
-        test.Calculate(P3clust.xmcdaVersion.V3, "/home/mateusz/Development/mcdm/P3CLUST/test/v3/", "/home/mateusz/Development/mcdm/P3CLUST/test/v3out/");
+        test.Calculate(P3clust.xmcdaVersion.V2, "/home/mateusz/Development/mcdm/P3CLUST/test/v2/", "/home/mateusz/Development/mcdm/P3CLUST/test/v2out/");
     }
 }
